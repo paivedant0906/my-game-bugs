e8e41f96c0d85906e704bd95682f150addb17465
paivedant0906 <74344625+paivedant0906@users.noreply.github.com>
2021-03-04 11:29:01 +0530

    Update sketch.js

--- sketch.js
+++ sketch.js
@@ -19,11 +19,16 @@
   var name,name1;
   var ei1,ei2;
   var score=0;
-  var safeprecent=0;
-  var dangerprecent=0;
+  var safepercent=0;
+  var life=3;
   var bottle,bottleimage;
-
-  // function to load Images.
+  var jumpsound
+  var winsound
+  var bombblastsound
+  var morei,hearti;
+  var n1,n2,n3;
+  var nutrient,nutrientsGroup;
+  // function to load Images and sounds.
   function preload(){
     background1image=loadImage("bg.jpg");
     playerimage=loadAnimation("1.png","2.png","3.png","4.png","5.png","6.png","7.png","8.png","9.png","10.png");
@@ -37,9 +42,19 @@
     start=loadImage("start.jpg");
     abouti=loadImage("about.png");
     homei=loadImage("home.png");
-    ei1=loadImage("Earthhappy.png");
+    ei1=loadImage("EARTHHAPPY.png");
     ei2=loadImage("sadearthmain.png");
     bottleimage=loadImage("energydrink.png");
+    jumpsound=loadSound("jump.mp3")
+    winsound=loadSound("win.mp3")
+    bombblastsound=loadSound("bombblast.mp3")
+    morei=loadImage("ReadMore.png");
+    hearti=loadImage("heart.png");
+    n1=loadImage("water.png");
+    n2=loadImage("seed.png");
+    n3=loadImage("ozone.png");
+    reseti=loadImage("reset.png")
+
   }
 
   function setup() {
@@ -58,48 +73,7 @@
     background2.addImage("bg2",background1image);
     background2.scale=1;
 
-    // creating borders for health.
-    healthholder=createSprite(displayWidth-350,50,300,10);
-
-    healthholder2=createSprite(displayWidth-350,100,300,10);
-
-    healthholder3=createSprite(displayWidth-200,75,10,60);
-
-    healthholder4=createSprite(displayWidth-500,75,10,60);
-
-    // giving colours to healthhlders.
-    healthholder.shapeColor="black";
-
-    healthholder2.shapeColor="black";
-
-    healthholder3.shapeColor="black";
-
-    healthholder4.shapeColor="black";
     
-    //  creating safe percent block.
-    healthb1=createSprite(displayWidth-350,75,300,50);
-
-    //giving colour to safe percent block1.
-    healthb1.shapeColor="blue";
-
-    // creating safe percent block safe percent block2.
-    healthb2=createSprite(displayWidth-350,75,225,50);
-
-   
-    //giving colour to safe percent block2.
-    healthb2.shapeColor="blue";
-    
-     //creating safe percent block3.
-    healthb3=createSprite(displayWidth-350,75,150,50);
-
-    //giving colour to safe percent block3.
-    healthb3.shapeColor="blue";
-    
-    // creating safe percent block4.
-    healthb4=createSprite(displayWidth-350,75,75,50);
-
-    //giving colour to safe percent block4.
-    healthb4.shapeColor="blue";
      
     //creating player and giving animation.
     player=createSprite(100,displayHeight/2+140,50,50);
@@ -110,6 +84,9 @@
      // creating home button  and adding image to it.
     home=createSprite(displayWidth-350,50,50,50);
     home.addImage(homei);
+
+    home1=createSprite(350,displayHeight-200,50,50);
+    home1.addImage(homei);
 
     // creating about button and adding image to it.
     about=createSprite(displayWidth/2,displayHeight/2,50,50);
@@ -133,10 +110,15 @@
     obstaclesGroup=new Group();
     bombGroup=new Group();
     bottleGroup=new Group();
+    nutrientsGroup=new Group();
   
-    
-  }
+   more=createSprite(displayWidth/2+340,displayHeight/2+100,50,50)
+   more.addImage(morei)
+   more.scale=0.5;
 
+   restart=createSprite(50,displayHeight-200)
+   restart.addImage(reseti)
+  }
   function draw(){
     // creating gamestates.
 
@@ -147,21 +129,15 @@
 
     // setting visibility of sprites.    
     home.visible=false;
+     home1.visible=false;
     player.visible=false;
     earth.visible=false;
     background1.visible=false;
     background2.visible=false;
     play.visible=true;
     about.visible=true;
-    healthholder.visible=false;
-    healthholder2.visible=false;
-    healthb1.visible=false;
-    healthb2.visible=false;
-    healthb3.visible=false;
-    healthb4.visible=false;
-    healthholder3.visible=false;
-    healthholder4.visible=false;
-
+    more.visible=false;
+     restart.visible=false
     // adding images to design start page.
     image(ei1,200,displayHeight/2-300,100,100);
     image(ei2,displayWidth/2-100,displayHeight/2+50,150,150);
@@ -200,10 +176,16 @@
     play.visible=false;
     about.visible=false;
     home.visible=true;
-
+      home1.visible=false;
+    more.visible=true;
+ restart.visible=false
     // giving instructions for pressing on home button.
     if(mousePressedOver(home)){
       gameState="start";  
+    }
+
+    if(mousePressedOver(more)){
+      gameState="more";  
     }
     
   drawSprites();
@@ -225,19 +207,8 @@
   text("Losing:",50,480);
   text("You lose if:",50,520)
   text("The player touches the obstacles four times to make the danger percentage to 100 or if the player blasts with the bomb",50,560);
-  text("OBSTACLES:",50,600);
-  text("BOMB,INDUSTRY,CAR,AXE.",50,640);
-  text("NUTRIENTS:",50,700);
-  text("seed,water,ozonemolecule,energy bottle",50,740);
-  text("INSTRUCTIONS:",50,780);
-  text("IF THE PLAYER TOUCHES THE BOTTLE THE SCORE INCREASES BY 2",50,820);
-  text("IF THE PLAYER TOUCHES ANY OF THE NUTRIENT EXCLUDING BOTTLE THE SAFE PERCENT INCREASES BY 25",50,860);
-  text("IF THE PLAYER TOUCHES ANY OF THE OBSTACLE EXCLUDING BOMB THE DANGER PERCENT INCREASES BY 25",50,900);
-  text("IF THE PLAYER TOUCHES  BOMB THE GAME IS OVER",50,940);
-  text("CONTROLS:",50,980);
-  text("PRESS SPACE TO JUMP",50,1020);
-  text("PRESS RIGHT TO MOVE RIGHT TO A CERTAIN EXTENT TO PROTECT FROM BOMB",50,1060);
-  text("PRESS LEFT TO MOVE RIGHT TO A CERTAIN EXTENT TO PROTECT FROM BOMB",50,1100);
+  
+  
    }
     
     // creating gamestate play.
@@ -252,13 +223,10 @@
     play.visible=false;
     about.visible=false;
     home.visible=false;
-    healthholder.visible=true;
-    healthholder2.visible=true;
-    healthb1.visible=true;
-    healthholder3.visible=true;
-    healthholder4.visible=true;
-    background1.visible=true;
-    background2.visible=true;
+    home1.visible=false;
+    background1.visible=true
+    background2.visible=true
+     restart.visible=false
 
     // setting earth's position according to player.
     earth.y=player.y-100;
@@ -268,7 +236,7 @@
     if(background1.x<0){
       background1.x=background1.width/2;
     }
-
+  
     if(background2.x<0){
       background2.x=background2.width/2;
     }
@@ -285,6 +253,7 @@
 
     if(keyDown("space")&& player.y>=320){
       player.velocityY=-12;
+      jumpsound.play()
     }
 
     // adding gravity to player.
@@ -296,13 +265,26 @@
     // giving instructions to do certain taks using if condition.
     if(bombGroup.isTouching(earth)){
       gameState="bombend";
+      bombblastsound.play()
+    }
+    
+     if(obstaclesGroup.isTouching(earth) || obstaclesGroup.isTouching(player)){
+      life=life-1;
+      obstaclesGroup.destroyEach();
+       safepercent-=25;
     }
     
     if(earth.isTouching(bottleGroup)){
       score=score+2;
       bottleGroup.destroyEach();
+      winsound.play()
     }
 
+   if(earth.isTouching(nutrientsGroup)){
+      safepercent+=25;
+      nutrientsGroup.destroyEach();
+       
+    }
     if(player.x>240){
       player.x=240;
     }
@@ -311,29 +293,89 @@
     }
    
   // displaying functions.
-  spawnenergy();
-  spawnobstacles();
+  //spawnenergy();
+  //spawnobstacles();
   spawnbomb();
-    
+ // spawnnutrients();
+
   drawSprites();
 
   // displaying score.
   textSize(40);
   fill("black");
-  text("SCORE:"+score,displayWidth/2,50);
+  text("SCORE:"+score,displayWidth/2-100,50);
+  text("SAFEPERCENT:"+safepercent+"%",displayWidth/2+100,50)
   }
 
   // creating gamestate bombend.
     else if(gameState==="bombend"){
     // giving image to background.
     background(background1image);
-
+    bombGroup.destroyEach()
+    nutrientsGroup.destroyEach()
+   bottleGroup.destroyEach()
+   obstaclesGroup.destroyEach()
+    drawSprites()
+    fill("BLACK")
+    textSize(40)
+       text("YOU LOSE",50,50)
+       text("YOU ARE BLASTED BY CARBON DIOXIDE BOMB",50,150)
+       text("THE EARTH IS GLOBALLY WARM NOW",50,250)
+       text("YOU WERE UNABLE TO SAVE THE EARTH",50,350)
+         restart.visible=true;
+         home1.visible=true;
+          home.visible=false;
+           play.visible=false;
+    about.visible=false;
+      more.visible=false;
+      player.visible=false;
+    earth.visible=false;
+    background1.visible=false
+    background2.visible=false
+        
+         if(mousePressedOver(restart)){
+      gameState="play"; 
+      frameCount=0; 
+    }
+       if(mousePressedOver(home1)){
+      gameState="start";  
+    }
     // displaying bombblast image.
-    image(bombblast,player.x,player.y-100,100,100);
+    image(bombblast,player.x,player.y-350,300,300);
+    image(ei2,player.x,player.y-100,300,300);
+    player.y=displayHeight/2
+    player.x=displayWidth/2+250
 
     }
+    else if(gameState==="more"){
+      if(mousePressedOver(home)){
+      gameState="start";  
+    }
+      background("white");
+       drawSprites();
+       fill("black")
+      textSize(20);
+      text("OBSTACLES:",50,50);
+text("BOMB,INDUSTRY,CAR,AXE.",50,100);
+  text("NUTRIENTS:",50,150);
+  text("seed,water,ozonemolecule,energy bottle",50,200);
+  text("INSTRUCTIONS:",50,250);
+  text("IF THE PLAYER TOUCHES THE BOTTLE THE SCORE INCREASES BY 2",50,300);
+  text("IF THE PLAYER TOUCHES ANY OF THE NUTRIENT EXCLUDING BOTTLE THE SAFE PERCENT INCREASES BY 25",50,350);
+  text("IF THE PLAYER TOUCHES ANY OF THE OBSTACLE EXCLUDING BOMB THE LIFE WILL BE DECREASED BY 1",50,400);
+  text("IF THE PLAYER TOUCHES  BOMB THE GAME IS OVER",50,450);
+  text("CONTROLS:",50,500);
+  text("PRESS SPACE TO JUMP",50,550);
+  text("PRESS RIGHT TO MOVE RIGHT TO A CERTAIN EXTENT TO PROTECT FROM BOMB",50,600);
+  text("PRESS LEFT TO MOVE RIGHT TO A CERTAIN EXTENT TO PROTECT FROM BOMB",50,650);
+    play.visible=false;
+    about.visible=false;
+    home.visible=true;
+     home1.visible=false;
+    more.visible=false;
+     restart.visible=false
   }
-
+  }
   // function to spawn obstacles at a certain framecount.
   function spawnobstacles(){
 
@@ -413,3 +455,42 @@
     bottleGroup.add(bottle);
     }
   }
+  function spawnnutrients(){
+    if(frameCount%100===0){
+    nutrient=createSprite(displayWidth+20,displayHeight/2+120,50,50)
+    nutrient.velocityX=-6;
+    var nurand=Math.round(random(1,3));
+
+    switch(nurand){
+        case 1:nutrient.addImage(n1)
+               nutrient.scale=0.5
+               nutrient.y=displayHeight/2+100
+        break;
+        case 2:nutrient.addImage(n2)
+               nutrient.scale=0.3
+        break;
+        case 3 :nutrient.addImage(n3)
+                nutrient.scale=0.3
+                nutrient.y=displayHeight/2+100
+        break;
+        default:break
+      
+      nutrientsGroup.add(nutrient);
+  }
+    }
+  }
+
+
+
+
+
+
+
+ /*nutrient.addImage(n1)
+        nutrient.scale=0.5
+        nutrient.y=displayHeight/2+100*/
+        
+
+        /*nutrient.addImage(n2)
+               nutrient.scale=0.3*/
+
